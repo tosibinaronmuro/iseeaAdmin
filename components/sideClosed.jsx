1b9fa27be8dd05e9739b04fbcb5b98cb979405ff
tosiron <tosironj@gmail.com>
2023-08-03 09:34:42 +0100

    minor changes

--- components/sideClosed.jsx
+++ components/sideClosed.jsx
@@ -31,26 +31,22 @@ const SideClosed = ({ handleIsNavOpen }) => {
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 opacity-75"
+                    className="h-5 w-5 opacity-75 "
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  strokeWidth="2"
+                    strokeWidth="2"
                   >
                     <path
-                      strokeLinecap="round"
-                    strokeLinejoin="round"
-                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                    />
-                    <path
-                      strokeLinecap="round"
-                    strokeLinejoin="round"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m6.072 10.072 2 2 6-4m3.586 4.314.9-.9a2 2 0 0 0 0-2.828l-.9-.9a2 2 0 0 1-.586-1.414V5.072a2 2 0 0 0-2-2H13.8a2 2 0 0 1-1.414-.586l-.9-.9a2 2 0 0 0-2.828 0l-.9.9a2 2 0 0 1-1.414.586H5.072a2 2 0 0 0-2 2v1.272a2 2 0 0 1-.586 1.414l-.9.9a2 2 0 0 0 0 2.828l.9.9a2 2 0 0 1 .586 1.414v1.272a2 2 0 0 0 2 2h1.272a2 2 0 0 1 1.414.586l.9.9a2 2 0 0 0 2.828 0l.9-.9a2 2 0 0 1 1.414-.586h1.272a2 2 0 0 0 2-2V13.8a2 2 0 0 1 .586-1.414Z"
                     />
                   </svg>
-
                   <span className="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100">
-                   Numbers
+                    Numbers
                   </span>
                 </a>
               </li>
@@ -65,16 +61,16 @@ const SideClosed = ({ handleIsNavOpen }) => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  strokeWidth="2"
+                    strokeWidth="2"
                   >
                     <path
                       strokeLinecap="round"
-                    strokeLinejoin="round"
+                      strokeLinejoin="round"
                       d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
                     />
                     <path
                       strokeLinecap="round"
-                    strokeLinejoin="round"
+                      strokeLinejoin="round"
                       d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                     />
                   </svg>
@@ -96,7 +92,7 @@ const SideClosed = ({ handleIsNavOpen }) => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  strokeWidth="2"
+                    strokeWidth="2"
                   >
                     <path d="M18 0H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h3.546l3.2 3.659a1 1 0 0 0 1.506 0L13.454 14H18a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-8 10H5a1 1 0 0 1 0-2h5a1 1 0 1 1 0 2Zm5-4H5a1 1 0 0 1 0-2h10a1 1 0 1 1 0 2Z" />
                   </svg>
@@ -117,7 +113,7 @@ const SideClosed = ({ handleIsNavOpen }) => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  strokeWidth="2"
+                    strokeWidth="2"
                   >
                     <path d="M19 0H1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1ZM2 6v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6H2Zm11 3a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0h2a1 1 0 0 1 2 0v1Z" />
                   </svg>
@@ -128,7 +124,8 @@ const SideClosed = ({ handleIsNavOpen }) => {
                 </a>
               </li>
               <li>
-                <a href="/pages/team"
+                <a
+                  href="/pages/team"
                   className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
                 >
                   <svg
@@ -137,11 +134,11 @@ const SideClosed = ({ handleIsNavOpen }) => {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                  strokeWidth="2"
+                    strokeWidth="2"
                   >
                     <path
                       strokeLinecap="round"
-                    strokeLinejoin="round"
+                      strokeLinejoin="round"
                       d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
                     />
                   </svg>
@@ -168,11 +165,11 @@ const SideClosed = ({ handleIsNavOpen }) => {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-            strokeWidth="2"
+              strokeWidth="2"
             >
               <path
                 strokeLinecap="round"
-              strokeLinejoin="round"
+                strokeLinejoin="round"
                 d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
               />
             </svg>
